708426f544ac3460dcc0b587b5ac998ec7f70931
guillesotelo <guille.sotelo.cloud@gmail.com>
2022-06-09 16:10:06 -0300

    modified res.sendFile

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('build'))
   app.get('*', (req, res) => {
-    req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
   })
 }
 
