1e178c8766a73da75e8c546f261d3afb1dd2a749
Guillermo Sotelo <guillermo@Guillermos-MacBook-Air.local>
2022-07-25 08:46:30 -0300

    fixing canva render 8

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -240,22 +240,7 @@ export default function Home() {
         if (saved && saved.status === 200) toast.success('Gasto guardado!')
         else toast.error('Error al guardar')
 
-        setTimeout(() => getAllMovements(submitData), 500)
-
-        setData({
-          ...data,
-          pay_type: allPayTypes[0],
-          category: allCategories[0],
-          author: allUsers[0],
-          amount: '',
-          detail: '',
-          date: new Date(),
-          ledger: ledger.id,
-          user: user.email
-        })
-        setOpenModal(false)
-        setIsEdit(false)
-        setCheck(-1)
+        setTimeout(() => history.go(0), 1500)
       }
       else toast.error('Revisa los campos')
     } catch (err) { toast.error('Error al guardar') }
