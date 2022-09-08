98bd2066db687a151a35e9d050419de19587b3d4
Guillermo Sotelo <guillermo.sotelo@conexiogroup.com>
2022-09-08 11:24:16 -0300

    various desktop fixes

--- src/components/PolarChart/index.js
+++ src/components/PolarChart/index.js
@@ -9,12 +9,10 @@ export default function PolarChart(props) {
         chartData
     } = props
 
-    const pieWidth = window.innerWidth - 180
-
     return (
         <div className='polarchart-container'>
             <h4 className='table-title'>{title || ''}</h4>
-            <PolarArea width={pieWidth} height={pieWidth} data={chartData}/>
+            <PolarArea data={chartData}/>
         </div>
     )
 }
